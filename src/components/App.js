85180f2f035670bb70d354b387ae154ded199e90
Unknown <finnstolze@googlemail.com>
2019-02-12 11:56:50 +0100

    installed React-Router and history with Home Component

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,16 @@
 import React from "react";
+import { Router, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import history from "../history";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <Router history={history}>
+        <Route path="/" exact component={Home} />
+      </Router>
+    </div>
+  );
 };
 
 export default App;
