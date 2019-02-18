3c25ce1252ae3e571c989f02eacad15231f8099b
Finn <finnstolze@posteo.de>
2019-02-18 10:45:38 +0100

    Updates

---
+++ precache-manifest.dcaadd681acfd21d066fea2ce2615aa5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c113afd6cb0d1a86d85f",
+    "url": "/spotifyandreact/static/js/main.c113afd6.chunk.js"
+  },
+  {
+    "revision": "020b61582156e9170d59",
+    "url": "/spotifyandreact/static/js/runtime~main.020b6158.js"
+  },
+  {
+    "revision": "52a9d2c88b024f12b7a4",
+    "url": "/spotifyandreact/static/css/2.77644afc.chunk.css"
+  },
+  {
+    "revision": "52a9d2c88b024f12b7a4",
+    "url": "/spotifyandreact/static/js/2.52a9d2c8.chunk.js"
+  },
+  {
+    "revision": "8c495153beb825f88faafc412ca609bb",
+    "url": "/spotifyandreact/index.html"
+  }
+];
