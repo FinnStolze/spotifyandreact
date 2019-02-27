f348c3b0789151c08e43e7fc08ac1db528e2bdea
Finn <finnstolze@posteo.de>
2019-02-27 16:01:02 +0100

    changed to Browser Router

--- src/components/App.js
+++ src/components/App.js
@@ -5,11 +5,7 @@ import history from "../history";
 
 const App = () => {
   return (
-<<<<<<< HEAD
     <BrowserRouter basename={process.env.PUBLIC_URL} history={history}>
-=======
-    <Router basename={process.env.PUBLIC_URL} history={history}>
->>>>>>> sdkPlayer
       <div>
         <Route path="/" exact component={Home} />
       </div>
